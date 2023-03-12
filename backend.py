6b247a55b8b346e6191fcf6f201595ec9b9f7a93
faux <118144566+fauxcat@users.noreply.github.com>
2023-03-12 14:40:44 +0000

    documentation, config window,  devices on label

--- backend.py
+++ backend.py
@@ -1,37 +1,80 @@
 class SmartPlug():
+    """
+    A class to represent a smart plug.
+    A smart plug has a consumption rate and is switched on or off.
+    The consumtion rate is 0 by default.
+    The smart plug is switched off by default.
+    """
 
     def __init__(self, switchedOn=False, consumptionRate=0):
         self.switchedOn = switchedOn
         self.consumptionRate = consumptionRate
 
     def getSwitchedOn(self):
+        """
+        Returns the switched on/off status of the smart plug.
+        """
         return self.switchedOn
 
     def getConsumptionRate(self):
+        """
+        Returns the consumption rate of the smart plug.
+        """
         return self.consumptionRate
 
     def toggleSwitch(self):
+        """
+        Toggles the status of the smart plug.
+        If the smart plug is switched off, this function will turn the switch on and vice versa.
+        """
         self.switchedOn = not self.switchedOn
 
     def setConsumptionRate(self, newConsumptionRate):
-        if (0 <= newConsumptionRate <= 150):
+        """
+        Sets the consumption rate of the smart plug to the given value.
+        If newConsumptionRate is not within the range of 0 and 150, the consumption rate is not changed.
+        In the event of an invalid consumption rate, an error is printed.
+        """
+        if (0 <= newConsumptionRate and newConsumptionRate <= 150):
             self.consumptionRate = newConsumptionRate
+        else:
+            print("Invalid consumption rate, must be between 0 and 150")
         
     def __str__(self):
         output = "Plug: Switched on = {}, {} consumption".format(self.switchedOn, self.consumptionRate)
         return output
 
 class SmartLight(SmartPlug):
+    """
+    A class to represent a smart light.
+    A smart light has a brightness and is switched on or off.
+    The brightness is 0 by default.
+    The smart light is switched off by default.
+    """
     def __init__(self, switchedOn=False, brightness=0):
+        """
+        The switchedOn attribute from smart plug is inherited to this class.
+        Therefore the getter and setter methods are also inherited, meaning they do not need to be defined here again.
+        """
         super().__init__(switchedOn)
         self.brightness = brightness
     
     def getBrightness(self):
+        """
+        Returns the brightness of the smart plug.
+        """
         return self.brightness
     
     def setBrightness(self, newBrightness):
-        if (0 <= newBrightness <= 100):
+        """
+        Sets the brightness of the smart light to the given value.
+        If newBrightness is not within the range of 0 and 100, the brightness is not changed.
+        In the event of an invalid brightness, an error is printed.
+        """
+        if (0 <= newBrightness and newBrightness <= 100):
             self.brightness = newBrightness
+        else:
+            print("Invalid brightness, must be between 0 and 100")
 
     def __str__(self):
         output = "Light: Switched on = {}, {} brightness".format(self.switchedOn, self.brightness)
@@ -39,26 +82,48 @@ class SmartLight(SmartPlug):
         return output
 
 class SmartHome():
+    """
+    A class to represent a smart home.
+    A smart home stores a list of smart plugs and smart lights.
+    """
     def __init__(self):
         self.devices = []
 
     def getDevices(self):
+        """
+        Returns every smart device in the smart home.
+        """
         return self.devices
 
     def getDeviceAt(self, index):
+        """
+        Returns the smart device with the given index in the smart home.
+        """
         return self.devices[index]
 
     def addDevice(self, device):
+        """
+        Adds a smart device to the smart home, at the end of the list.
+        """
         self.devices.append(device)
 
     def toggleSwitch(self, index):
+        """
+        Toggles the on/off status of the smart device at the given index.
+        """
         self.devices[index].toggleSwitch()
 
     def turnOnAll(self):
+        """
+        Changes all of the smart devices' on/off statuses to on, regardless of their previous status.
+        """
         for i in range(len(self.devices)):
             self.devices[i].switchedOn = True
 
     def turnOffAll(self):
+        """
+        Changes all of the smart devices' on/off statuses to off, regardless of their previous status.
+        """
         for i in range(len(self.devices)):
             self.devices[i].switchedOn = False
 
@@ -69,6 +134,12 @@ class SmartHome():
         return output
 
 def testSmartPlug():
+    """
+    Tests the smart plug class.
+    First, a plug is created with the SmartPlug class.
+    The plug is switched from its default value of off to on.
+    The setConsumptionRate method is then tested by using a non-integer value as an input parameter.
+    """
     plug = SmartPlug()
 
     plug.toggleSwitch()
@@ -80,6 +151,12 @@ def testSmartPlug():
     print(plug.getConsumptionRate())
 
 def testSmartLight():
+    """
+    Tests the smart light class.
+    First, a light is created with the SmartLight class.
+    The light is switched from its default value of off to on.
+    The setBrightness method is then tested by using an integer value as an input parameter.
+    """
     light = SmartLight()
 
     light.toggleSwitch()
@@ -92,6 +169,14 @@ def testSmartLight():
     print(light)
 
 def testSmartHome():
+    """
+    Tests the smart home class.
+    First, a home is created with the SmartHome class.
+    Several smart devices are created using their respective classes.
+    Some of the devices are edited with their own methods.
+    All devices are added to the list in the home with the addDevice method.
+    Every device that has now been added to the home is now switched on.
+    """
     home = SmartHome()
     plug1 = SmartPlug()
     plug2 = SmartPlug()
@@ -110,5 +195,3 @@ def testSmartHome():
     home.turnOnAll()
 
     print(home)
-
-
--- frontend.py
+++ frontend.py
@@ -4,9 +4,13 @@ from backend import *
 
 
 mainWin = Tk()
-
+global devicesOn
+devicesOn = 0
 
 def setupHome():
+    """
+    Creates a new home and adds the correct devices for my UP number(s).
+    """
     global home 
     home = SmartHome()
     plug1_1 = SmartPlug()
@@ -23,8 +27,13 @@ def setupHome():
 
 
 def setupMainWin():
+    """
+    Setup `mainWin` to display the home and its devices.
+    Display two buttons that turn on and turn off all devices.
+    Calls function to display the devices.
+    """
     mainWin.title("Smart Home")
-    mainWin.geometry("500x350")
+    mainWin.geometry("600x350")
     mainWin.resizable(False, False)
     mainWin.columnconfigure(index=0, weight=2)
 
@@ -38,6 +47,12 @@ def setupMainWin():
     mainWin.mainloop()
 
 def listDevices():
+    """
+    For each device in `home`, create a text widget, and two buttons on the `mainWin`.
+    The text contains the device's information.
+    One button is used to toggle the device.
+    The other button is used to configure the device.
+    """
     global textList
     textList = []
     global deviceIndex
@@ -47,33 +62,136 @@ def listDevices():
         global deviceTxt
         deviceTxt = Text(mainWin, height=1, width=50)
         deviceTxt.insert("1.0", str(device))
-        deviceTxt.grid(row = deviceIndex+2, column = 0, padx=10, pady=10)
+        deviceTxt.grid(row=deviceIndex+2, column=0, padx=10, pady=10, sticky="w")
         textList.append(deviceTxt)
 
         toggleBtn = Button(mainWin, text="Toggle this", command=lambda index=deviceIndex: togglePressed(index))
         toggleBtn.grid(row=deviceIndex+2, column=1, padx=10, pady = 10, sticky="e")
 
+        def configCmd(i=deviceIndex):
+            configWindow(i)
+        
+        configBtn = Button(mainWin, text="Configure", command=configCmd)
+        configBtn.grid(row=deviceIndex+2, column=2, padx=10, pady = 10, sticky="e")
+
+    global devicesOnLabel
+    devicesOnLabel = Label(mainWin, text=("Number of devices turned on: {}").format(devicesOn))
+    devicesOnLabel.grid(row=7, column=0, padx=10, sticky="w")
+
 
 def reListDevices():
+    """
+    After any changes are made to the devices, this function is used instead of listDevices.
+    This is because there is no need to draw every widget again over the initially drawn widgets.
+    """
     for deviceIndex in range(5):
         device = home.getDeviceAt(deviceIndex)
 
         textList[deviceIndex].delete("1.0", "2.0")
         textList[deviceIndex].insert("1.0", str(device))
+    
+    devicesOnLabel.config(text = ("Number of devices turned on: {}").format(devicesOn))
+    
 
+
+def configWindow(deviceIndex):
+    """
+    Create a new window to configure the device at `deviceIndex` of `home`.
+    The window will contain a text, showing the product's information.
+    The window will also contain a cancel button to close the config window.
+    The window will also contain an entry to change the respective integer value for each device.
+    The new value entered into the window can be used to configure the device with the submit button if it is a valid input.
+    When the change is submitted, the list of devices in `mainWin` will update.
+    """
+    configWin = Toplevel()
+    configWin.geometry("400x150")
+    configWin.resizable(False, False)
+    
+    device = home.getDeviceAt(deviceIndex)
+    configWin.title("Configure Smart Device")
+
+    titleLabelTxt = Label(configWin, text="Configure Smart Device")
+    titleLabelTxt.grid(row=0, column=0, padx=10, sticky="w")
+    titleLabelTxt.config(font=("TkDefaultFont", 12, "bold"))
+
+    deviceTxt = Text(configWin, height=1, width=45)
+    deviceTxt.insert("1.0", str(device))
+    deviceTxt.grid(row=1, column=0,columnspan=2, padx=10, pady=10, sticky="w")
+
+    inputEntry = Entry(configWin)
+    inputEntry.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+
+    def submitCmd():
+        errorLabel = Label(configWin, text="")
+        errorLabel.destroy()
+
+        if type(device) is SmartPlug:
+            try:
+                if 0 <= int(inputEntry.get()) <= 150:
+                    newConsumptionRate = int(inputEntry.get())
+                    device.setConsumptionRate(newConsumptionRate)
+                    reListDevices()
+                    configWin.destroy()
+                else:
+                    errorLabel = Label(configWin, text="Please enter an integer between 0 and 150")
+                    errorLabel.grid(row=3, column=0, padx=75, pady=10, sticky="w")
+            except ValueError:
+                errorLabel = Label(configWin, text="Please enter an integer")
+                errorLabel.grid(row=3, column=0, padx=75, pady=10, sticky="w")
+        elif type(device) is SmartLight:
+            try:
+                if 0 <= int(inputEntry.get()) <= 100:
+                    newBrightness = int(inputEntry.get())
+                    device.setBrightness(newBrightness)
+                    reListDevices()
+                    configWin.destroy()
+                else:
+                    errorLabel = Label(configWin, text="Please enter an integer between 0 and 100")
+                    errorLabel.grid(row=3, column=0, padx=75, pady=10, sticky="w")
+            except ValueError:
+                errorLabel = Label(configWin, text="Please enter an integer")
+                errorLabel.grid(row=3, column=0, padx=75, pady=10, sticky="w")
+
+    submitBtn = Button(configWin, text="Submit", command=submitCmd)
+    submitBtn.grid(row=2, column=0, padx=150, sticky="w")
+
+    cancelBtn = Button(configWin, text="Cancel", command=configWin.destroy)
+    cancelBtn.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+
+
+    
 
 def allOffPressed():
+    """
+    Turns every device in the home off, regardless of its previous state.
+    """
+    global devicesOn
     home.turnOffAll()
+    devicesOn = 0
     reListDevices()
 
 def allOnPressed():
+    """
+    Turns every device in the home on, regardless of its previous state.
+    """
+    global devicesOn
     home.turnOnAll()
+    devicesOn = 5
     reListDevices()
 
 def togglePressed(index):
-    
+    """
+    Toggles the device at the given index on/off.
+    """
+    global devicesOn
     device = home.getDeviceAt(index)
     device.toggleSwitch()
+
+    if device.getSwitchedOn() == False:
+        devicesOn -= 1
+    else:
+        devicesOn += 1
     reListDevices()
 
 def main():
